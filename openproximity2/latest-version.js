b7e134a4e2e1c7e83b47af5c4a29d9d9ae0a70ae
naranjo.manuel <none@none>
2009-05-23 15:37:31 +0000

    Let's define a function in the JS
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%4032

--- openproximity2/latest-version.js
+++ openproximity2/latest-version.js
@@ -1,1 +1,10 @@
-var LATEST_VERSION="05152009";
+function compare_version(CURRENT_VERSION) {
+    var LATEST_VERSION="05152009";
+    
+    if ( CURRENT_VERSION == LATEST_VERSION ){
+	alert("Running latest version");
+    }
+    else {
+	alert("New version available");
+    }
+}
