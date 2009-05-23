b965ff1f75f3c543759572cff705081636e38682
naranjo.manuel <none@none>
2009-05-23 15:41:45 +0000

    ok function works, now we just return the compare result
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%4033

--- openproximity2/latest-version.js
+++ openproximity2/latest-version.js
@@ -1,10 +1,5 @@
 function compare_version(CURRENT_VERSION) {
     var LATEST_VERSION="05152009";
     
-    if ( CURRENT_VERSION == LATEST_VERSION ){
-	alert("Running latest version");
-    }
-    else {
-	alert("New version available");
-    }
+    return CURRENT_VERSION == LATEST_VERSION
 }
