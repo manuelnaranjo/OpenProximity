8cfa98a7dc344512bc82876721127cb0a482712a
naranjo.manuel <none@none>
2009-05-31 19:44:16 +0000

    Changing latest release version number, preparing for alpha 2
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%4036

--- openproximity2/latest-version.js
+++ openproximity2/latest-version.js
@@ -1,5 +1,5 @@
 function compare_version(CURRENT_VERSION) {
-    var LATEST_VERSION="05152009";
+    var LATEST_VERSION="05312009";
     var a = new Array();
     a.new_available = CURRENT_VERSION != LATEST_VERSION
     a.latest = LATEST_VERSION
