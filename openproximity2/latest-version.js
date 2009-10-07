a5814b52a577848d6aa4853b4d2da189de90575b
naranjo.manuel <none@none>
2009-10-07 18:36:22 +0000

    modified:   latest-version.js * Do case insensitive comparison for version number
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%4092

--- openproximity2/latest-version.js
+++ openproximity2/latest-version.js
@@ -3,8 +3,8 @@ var message_show = 0
 function compare_version(CURRENT_VERSION) {
     var LATEST_VERSION="beta-10062009";
     var a = new Array();
-    a.new_available = CURRENT_VERSION != LATEST_VERSION
-    a.latest = LATEST_VERSION
+    a.new_available = CURRENT_VERSION.toLowerCase() != LATEST_VERSION.toLowerCase()
+    a.latest = LATEST_VERSION.toLowerCase()
     if (a.new_available){
         if (message_show==0){
            alert("There's a new version of OpenProximity2 available, for more information on how to upgrade visit http://code.google.com/p/proximitymarketing/wiki/Updates")
