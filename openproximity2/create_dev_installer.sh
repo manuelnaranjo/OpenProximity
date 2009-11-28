4a2c9601dcc1662c7e8b6c860369df65a2f6b8ee
naranjo.manuel <none@none>
2009-11-28 18:02:50 +0000

    Tag point for devel release: devel-11282009150144
    modified:   create_dev_installer.sh
    * Add echo of devel-version file
    
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40149

--- openproximity2/create_dev_installer.sh
+++ openproximity2/create_dev_installer.sh
@@ -7,5 +7,6 @@ rm openproximity2/latest-version
 VERSION="devel-$(date +%m%d%Y%H%M%S)"
 
 echo $VERSION > openproximity2/latest-version
+echo $VERSION > ../devel-version
 
 tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$VERSION.tar.gz openproximity2
