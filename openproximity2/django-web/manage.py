c28e2f9ded8d8de3838d54ebc57fc3a61813c721
naranjo.manuel <none@none>
2009-12-22 13:40:11 +0000

    modified:   django-web/manage.py modified:   prepare_installer.sh * patches from luisao, issue #15: http://code.google.com/p/proximitymarketing/issues/detail?id=15
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40154

--- openproximity2/django-web/manage.py
+++ openproximity2/django-web/manage.py
@@ -20,9 +20,12 @@ from django.core.management import execute_manager, setup_environ
 try:
     import settings # Assumed to be in the same directory.
     setup_environ(settings)
-except ImportError:
-    import sys
+except ImportError, e:
+    import sys, traceback
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+    sys.stderr.write("Description: %s\n" % str(e))
+    sys.stderr.write("Traceback:\n")
+    traceback.print_exc()
     sys.exit(1)
 
 from net.aircable.openproximity.pluginsystem import pluginsystem
--- openproximity2/prepare_installer.sh
+++ openproximity2/prepare_installer.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# echo all commands if debug is enabled
+if [ -n "$DEBUG" ]; then
+    set -x
+fi
+
+# exit script if we try to use an uninitialised variable
+set -u
+
+# exit if we get an error
+set -e
+
 function download_and_uncompress(){
     PACKAGE=$1
     VERSION=$2
@@ -9,13 +20,13 @@ function download_and_uncompress(){
     echo "processing $PACKAGE"
     
     if [ ! -f $CWD/libs/$PACKAGE-$VERSION.tar.gz ]; then
-	echo "Downloading"
-	wget -O $CWD/libs/$PACKAGE-$VERSION.tar.gz $URL/$PACKAGE-$VERSION.tar.gz
+        echo "Downloading"
+        wget -P $CWD/libs $URL/$PACKAGE-$VERSION.tar.gz
     fi
     
     echo "extracting $PACKAGE"
     gunzip -c $CWD/libs/$PACKAGE-$VERSION.tar.gz | tar -x
-    (cd $PACKAGE-$VERSION; cp -r $FOLDER $LIB_TARGET)
+    cd $PACKAGE-$VERSION; cp -r $FOLDER $LIB_TARGET
 }
 
 function download_egg(){
@@ -26,37 +37,61 @@ function download_egg(){
     echo "processing $PACKAGE"
     
     if [ ! -f $CWD/libs/$PACKAGE-$VERSION.egg ]; then
-	echo "Downloading"
-	wget -O $CWD/libs/$PACKAGE-$VERSION.egg $URL/$PACKAGE-$VERSION.egg
+        echo "Downloading"
+        wget -P $CWD/libs $URL/$PACKAGE-$VERSION.egg
     fi
     cp $CWD/libs/$PACKAGE-$VERSION.egg $LIB_TARGET
+}
+
+function git_download(){
+    PACKAGE=$1
+    VERSION=$2
+    GIT=$3
+    COMMIT=$4
+    
+    echo "processing $PACKAGE"
+
+    if [ ! -d $CWD/libs/$PACKAGE-$VERSION ]; then
+        echo "Downloading"
+        pushd $CWD/libs
+        git clone $GIT $PACKAGE
+        cd $PACKAGE
+        git checkout $COMMIT
+        popd
+        mv $CWD/libs/$PACKAGE $CWD/libs/$PACKAGE-$VERSION
+    fi
+    cp -r $CWD/libs/$PACKAGE-$VERSION/$PACKAGE $LIB_TARGET
 }
 
 function git_egg(){
     PACKAGE=$1
     VERSION=$2
     GIT=$3
+    COMMIT=$4
     
     echo "processing $PACKAGE"
     
     if [ ! -f $CWD/libs/$PACKAGE-$VERSION.egg ]; then
-	echo "Downloading"
-	git clone $GIT $PACKAGE
-	cd $PACKAGE
-	python setup.py bdist_egg
-	mv dist/$PACKAGE-$VERSION.egg $CWD/libs/$PACKAGE-$VERSION.egg
-	cd ..
-	rm -rf $PACKAGE.temp
+        echo "Downloading"
+        pushd $CWD/libs
+        git clone $GIT $PACKAGE
+        cd $PACKAGE
+        git checkout $COMMIT
+        python setup.py bdist_egg
+        mv dist/$PACKAGE-$VERSION.egg $CWD/libs/$PACKAGE-$VERSION.egg
+        cd ..
+        rm -rf $PACKAGE.temp
+        popd
     fi
     cp $CWD/libs/$PACKAGE-$VERSION.egg $LIB_TARGET
 }
 
 
-echo "Creating installer for version" $(cat latest-version)
+echo "Creating installer for version" `cat latest-version`
 
-OP2=$(pwd)/distrib/
+OP2=`pwd`/distrib/
 LIB_TARGET=${OP2}/openproximity2/libs
-CWD=$(pwd)
+CWD=`pwd`
 
 rm -rf $OP2
 mkdir -p $OP2/openproximity2
@@ -94,7 +129,7 @@ cd tmp
 
 download_and_uncompress rpyc 3.0.6 rpyc http://ufpr.dl.sourceforge.net/sourceforge/rpyc
 download_and_uncompress Django 1.1 django http://media.djangoproject.com/releases/1.1
-download_and_uncompress lincolnloop-django-cpserver 19739be django_cpserver http://github.com/lincolnloop/django-cpserver/tarball
+git_download django_cpserver 19739be git://github.com/lincolnloop/django-cpserver.git 19739be
 download_and_uncompress django-rosetta 0.4.7 rosetta http://django-rosetta.googlecode.com/files
 download_and_uncompress wadofstuff-django-serializers 1.0.0 wadofstuff http://wadofstuff.googlecode.com/files
 download_and_uncompress poster 0.4 poster http://pypi.python.org/packages/source/p/poster/
@@ -104,11 +139,11 @@ download_and_uncompress django-mailer 0.1.0 mailer http://pypi.python.org/packag
 
 cd $OP2
 
-for i in $(ls $CWD/patches/*.patch); do
-    (cd openproximity2/libs; patch -p0 < $i)
+for i in `ls $CWD/patches/*.patch`; do
+    cd "$LIB_TARGET"; patch -p0 < $i
 done
 
 rm -rf tmp
-(cd openproximity2; bash manager.sh compilemessages)
-rm -f $(find . | grep "\.pyc$")
-rm -f $(find . | grep "\.pyo$")
+cd "$OP2/openproximity2"; bash manager.sh compilemessages
+rm -f `find . | grep "\.pyc$"`
+rm -f `find . | grep "\.pyo$"`
