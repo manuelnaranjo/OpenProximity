25f87a00db4ff3e076a43c1f3e0adf1e6f4bc408
naranjo.manuel <none@none>
2010-01-09 14:03:54 +0000

    oops there's a typo there
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40177

---
+++ openproximity2/django-web/openproximity/statistics.py
@@ -0,0 +1,38 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# Create your views here.
+
+from django.db import transaction
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import Context
+from django.template.loader import get_template
+from django.utils import simplejson
+from django.views.generic import list_detail
+
+from django.conf import settings
+
+from pluginsystem import pluginsystem
+
+from re import compile
+from mimetypes import guess_type as guess_mime
+
+from models import *
+from forms import *
+import rpyc, os
+
+SET = settings.OPENPROXIMITY.getAllSettings()
+
