109ee73edde1bfccb73143203a549de2a41e29f1
naranjo.manuel <none@none>
2010-04-06 18:08:37 +0000

    modified:   serverXR/manager.py * fixed ping function, don't take into account select errors * fixed identation
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40264

--- openproximity2/serverXR/manager.py
+++ openproximity2/serverXR/manager.py
@@ -17,8 +17,8 @@
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
 # -*- coding: utf-8 -*-
-import dbus, dbus.glib, gobject
-import os, sys
+import dbus, dbus.glib, gobject, select
+import os, sys, errno
 import rpyc
 
 from rpyc import async
@@ -36,12 +36,16 @@ loop = None
 
 def ping():
     try:
-	server.ping(timeout=10000)
+        server.ping(timeout=10000)
+        return True
+    except select.error:
+	logger.warn("select.error")
 	return True
     except Exception, err:
-	logger.info("ping lost connection, cause: %s" % err)
-	stop()
-	
+        logger.info("ping lost connection, cause: %s, %s" % (err, type(err)))
+        logger.exception(err)
+        stop()
+    
 def stop():
     global manager
     from uploader import UploadManager
@@ -54,19 +58,15 @@ def exposed_ping():
     
 def handle_name_owner_changed(own, old, new):
     if own.startswith('org.bluez'):
-	if new is None or len(str(new))==0:
-	    logger.info( "bluez has gone down, time to get out")
-	else:
-	    logger.info( "bluez started, time to restart")
-	stop()
+        if new is None or len(str(new))==0:
+            logger.info( "bluez has gone down, time to get out")
+        else:
+            logger.info( "bluez started, time to restart")
+        stop()
 
 def handle_adapter_added_or_removed(path, signal):
     logger.info("bluez.%s: %s" % (signal, path))
     stop()
-
-#def handle_adapter_removed(path):
-#    logger.info("blue.AdapterRemoved: %s" % (path))
-#    loop.quit()
 
 def init():
     global manager, bus, loop, server
@@ -77,18 +77,22 @@ def init():
 
     a=list()
     for b in manager.manager.ListAdapters():
-	obj = bus.get_object('org.bluez', b)
-	adapter = dbus.Interface(obj, 'org.bluez.Adapter')
-	a.append(str(adapter.GetProperties()['Address']))
+        obj = bus.get_object('org.bluez', b)
+        adapter = dbus.Interface(obj, 'org.bluez.Adapter')
+        a.append(str(adapter.GetProperties()['Address']))
     logger.info("Connected dongles: %s" % a)
 
     if type == 'scanner':
-	server.root.scanner_register(stop, manager, a, exposed_ping)
+        server.root.scanner_register(stop, manager, a, exposed_ping)
     elif type == 'uploader':
-	server.root.uploader_register(stop, manager, a, exposed_ping)
+        server.root.uploader_register(stop, manager, a, exposed_ping)
     else:
-	server.root.generic_register(remote_quit=stop, client=manager, 
-		dongles=a, ping=exposed_ping)
+        server.root.generic_register(
+            remote_quit=stop, 
+            client=manager, 
+            dongles=a, 
+            ping=exposed_ping
+        )
     logger.info("exiting init")
 
     # stupid way to make rpyc do something
@@ -100,56 +104,63 @@ def run(server_, port, type_):
     global server, manager, bus, loop
     
     try:
-	server = rpyc.connect(server_, int(port))
+        server = rpyc.connect(server_, int(port))
     except:
-	import time
-	logger.info("server is not running")
-	time.sleep(10)
-	autoreload.RELOAD = True
-	sys.exit(3)
+        import time
+        logger.info("server is not running")
+        time.sleep(10)
+        autoreload.RELOAD = True
+        sys.exit(3)
 
     bus=dbus.SystemBus()
     gobject.threads_init()
     dbus.glib.init_threads()
     
-    bus.add_signal_receiver(handle_name_owner_changed,
-	'NameOwnerChanged',
-	'org.freedesktop.DBus',
-	'org.freedesktop.DBus',
-	'/org/freedesktop/DBus')
+    bus.add_signal_receiver(
+        handle_name_owner_changed,
+        'NameOwnerChanged',
+        'org.freedesktop.DBus',
+        'org.freedesktop.DBus',
+        '/org/freedesktop/DBus'
+    )
 
-    bus.add_signal_receiver(handle_adapter_added_or_removed,
-	signal_name='AdapterAdded',
-	dbus_interface='org.bluez.Manager',
-	member_keyword='signal')
+    bus.add_signal_receiver(
+        handle_adapter_added_or_removed,
+        signal_name='AdapterAdded',
+        dbus_interface='org.bluez.Manager',
+        member_keyword='signal' 
+    )
 
-    bus.add_signal_receiver(handle_adapter_added_or_removed,
-	signal_name='AdapterRemoved',
-	dbus_interface='org.bluez.Manager',
-	member_keyword='signal')
+    bus.add_signal_receiver(
+        handle_adapter_added_or_removed,
+        signal_name='AdapterRemoved',
+        dbus_interface='org.bluez.Manager',
+        member_keyword='signal'
+    )
 
     try:
-	if type_ == 'scanner':
-	    from scanner import ScanManager
-	    logger.info("init scanner")
-	    manager = ScanManager(bus, rpc=server)
-	elif type_ == 'uploader':
-	    logger.info("init uploader")
-	    from uploader import UploadManager
-	    manager = UploadManager(bus, rpc=server)
-	else:
-	    for i in pluginsystem.get_plugins('serverxr'):
-		if type_==i.provides['serverxr_type']:
-		    logger.info("init %s" % i.provides['serverxr_type'])
-		    manager = i.provides['serverxr_manager'](bus, rpc=server)
-		    break
-	if manager is None:
-	    raise Exception ("Not valid type")
-	gobject.timeout_add(100, init) # delay initialization 'til loop is running
+        if type_ == 'scanner':
+            from scanner import ScanManager
+            logger.info("init scanner")
+            manager = ScanManager(bus, rpc=server)
+        elif type_ == 'uploader':
+            logger.info("init uploader")
+            from uploader import UploadManager
+            manager = UploadManager(bus, rpc=server)
+        else:
+            for i in pluginsystem.get_plugins('serverxr'):
+                if type_==i.provides['serverxr_type']:
+                    logger.info("init %s" % i.provides['serverxr_type'])
+                    manager = i.provides['serverxr_manager'](bus, rpc=server)
+                    break
+        if manager is None:
+            raise Exception ("Not valid type")
+        gobject.timeout_add(100, init) # delay initialization 'til loop is running
     except dbus.DBusException, err:
-	logger.info("bluez isn't ready, delaying init")
+        logger.info("bluez isn't ready, delaying init")
+    
+    # start our loop and setup ping
     gobject.timeout_add(1000, ping)
-
     loop=gobject.MainLoop()
     loop.run()
 
@@ -164,11 +175,13 @@ if __name__ == '__main__':
     valid_modes=['scanner', 'uploader']
 
     pluginsystem.find_plugins()
-    for i in pluginsystem.get_plugins('serverxr'):
-	valid_modes.append(i.provides['serverxr_type'])
+    valid_modes.extend(
+        [ i.provides['serverxr_type'] 
+            for i in pluginsystem.get_plugins('serverxr') ] )
 
     if sys.argv[3] not in valid_modes:
         print "not valid type, you can use any of", valid_modes
         sys.exit(0)
 
     autoreload.main(run, tuple(sys.argv[1:4]),{})
+
