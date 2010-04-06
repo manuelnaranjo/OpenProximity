045ff65ecf8715cb0aa6e52582116b64939a6367
naranjo.manuel <none@none>
2010-04-06 18:06:55 +0000

    Upgraded copyright information, changed tabs to spaces to make the code more coherent
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40255

--- openproximity2/django-web/openproximity/management/commands/egg_command.py
+++ openproximity2/django-web/openproximity/management/commands/egg_command.py
@@ -1,9 +1,26 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2010,2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from django.core.management.base import BaseCommand
 from django.conf import settings
 from django.utils.importlib import import_module
 
 class Command(BaseCommand):
-    help = "try running a command inside an egg, pass as argument the wanted command"
+    help = "try running a command inside an egg, pass as argument the wanted "
+            "command"
     
     def handle(self, command="", *args, **kwargs):
 	for app in settings.INSTALLED_APPS:
