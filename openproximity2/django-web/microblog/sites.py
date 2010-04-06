b4b9af85b67b1f5d99a13d5afd1fe301a7085773
naranjo.manuel <none@none>
2010-04-06 18:06:28 +0000

    Added copyright information to microblog, changed tabs to spaces to make the code more coherent
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40253

--- openproximity2/django-web/microblog/sites.py
+++ openproximity2/django-web/microblog/sites.py
@@ -1,3 +1,19 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2010,2009 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from django.core import serializers
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
@@ -7,61 +23,58 @@ class AlreadyRegistered(Exception):
 
 class TweetSite(object):
     """
-	A tweet site
+        A tweet site
     """
     
     def __init__(self, klass, field="pk", convert=int):
-	'''
-	    Creates a new TweetSite.
-	    arguments:
-		klass: which class to use
-		field: which field to use for sorting
-		convert: converting function when using non integers variables
-	'''
-	self.klass = klass
-	self.field = field
-	self.convert = convert
-	
-	from django.conf.urls.defaults import patterns, url, include
-	
-	self.urlpatterns = patterns( '',
-	    url(r'^latest/', self.latest_view),
-	    url(r'^posts/', self.posts_view),
-	    url(r'.*', self.index_view), 
-	)
+        '''
+            Creates a new TweetSite.
+            arguments:
+                klass: which class to use
+                field: which field to use for sorting
+                convert: converting function when using non integers variables
+        '''
+        self.klass = klass
+        self.field = field
+        self.convert = convert
+        
+        from django.conf.urls.defaults import patterns, url, include
+        
+        self.urlpatterns = patterns( '',
+            url(r'^latest/', self.latest_view),
+            url(r'^posts/', self.posts_view),
+            url(r'.*', self.index_view), 
+        )
 
     def latest_view(self, request):
-	"""
-	    This view will return which is the latest entry in the table.
-	"""
-	data = serializers.serialize('json', (self.klass.objects.latest('pk'),) )
-	return HttpResponse(data, mimetype="application/json")
-	
+        """
+            This view will return which is the latest entry in the table.
+        """
+        data = serializers.serialize('json',(self.klass.objects.latest('pk'),))
+        return HttpResponse(data, mimetype="application/json")
+        
 
     def posts_view(self, request):
-	"""
-	    This view will return a json array with the latest posts
-	"""
-	
-	pk = request.GET.get('pk', None)
-	amount = int(request.GET.get('amount', 10))
-	
-	qs = self.klass.objects.all()
-	
-	if pk is not None:
-	    pk = self.convert(pk)
-	    
-	    filt = { '%s__gt' % self.field: pk }
-	
-	    qs = qs.filter(**filt)
-	qs = qs.order_by('-%s' % self.field)
-	
-	data = serializers.serialize('json', qs[:amount])
-	
-	return HttpResponse(data, mimetype="application/json")
-	
+        """
+            This view will return a json array with the latest posts
+        """
+        
+        pk = request.GET.get('pk', None)
+        amount = int(request.GET.get('amount', 10))
+        
+        qs = self.klass.objects.all()
+        
+        if pk is not None:
+            pk = self.convert(pk)
+            
+            filt = { '%s__gt' % self.field: pk }
+        
+            qs = qs.filter(**filt)
+        qs = qs.order_by('-%s' % self.field)
+        
+        data = serializers.serialize('json', qs[:amount])
+        
+        return HttpResponse(data, mimetype="application/json")
+        
     def index_view(self, request):
-	return render_to_response( 'microblog/base.html',
-	{
-	
-	})
+        return render_to_response( 'microblog/base.html',{})
--- openproximity2/django-web/microblog/templatetags/microblog.py
+++ openproximity2/django-web/microblog/templatetags/microblog.py
@@ -1,3 +1,19 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2010,2009 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 from django import template
 
 register = template.Library()
@@ -7,7 +23,14 @@ def MochiKit():
     return {}
 
 @register.inclusion_tag("log.html")
-def Tweet(element, field='pk', url='./', line_format='post.pk + " " + post.fields', interval=5000, amount=100, first_amount=5):
+def Tweet(element, 
+        field='pk', 
+        url='./', 
+        line_format='post.pk + " " + post.fields', 
+        interval=5000, 
+        amount=100, 
+        first_amount=5):
+
     return {
 	    'element': element,
 	    'field': field,
@@ -17,3 +40,4 @@ def Tweet(element, field='pk', url='./', line_format='post.pk + " " + post.field
 	    'line_format': line_format,
 	    'first_amount': first_amount,
 	}
+
