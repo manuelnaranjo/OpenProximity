ea2992b5f285a23a28322a9a66626cb656158b25
naranjo.manuel <none@none>
2010-04-06 18:07:20 +0000

    Upgraded copyright information, changed tabs to spaces to make the code more coherent
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40257

--- openproximity2/django-web/openproximity/templatetags/openproximity.py
+++ openproximity2/django-web/openproximity/templatetags/openproximity.py
@@ -1,3 +1,20 @@
+#    OpenProximity2.0 is a proximity marketing OpenSource system.
+#    Copyright (C) 2010,2009 Naranjo Manuel Francisco <manuel@aircable.net>
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation version 2 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 from django import template
 from django.conf import settings
 from net.aircable.openproximity.pluginsystem import pluginsystem
@@ -11,7 +28,7 @@ def do_settings(parser, token):
 
 class SettingsNode(template.Node):
     def render(self, context):
-	context['settings'] = SET
+        context['settings'] = SET
 	return ''
 
 register.tag('settings', do_settings)
@@ -21,14 +38,17 @@ def do_plugins(parser, token):
 
 class PluginsNode(template.Node):
     def render(self, context):
-	context['plugins'] = pluginsystem.get_plugins('urls')
+        context['plugins'] = pluginsystem.get_plugins('urls')
 	return ''
+
 register.tag('plugins', do_plugins)
 
 @register.simple_tag
 def createNavButton(ref, rid, text, style=None):
-    return '''<div class="nav_button" id="%s">
+    return '''
+<div class="nav_button" id="%s">
     <div class="inner">
     	    <a style="%s"  href="%s">%s</a>
     </div>
 </div>''' % (rid, style if style else ';', ref, text)
+
