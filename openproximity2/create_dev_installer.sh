11ea1698b99a71e0d1f62ae6878d870a0bfc70ff
naranjo.manuel <none@none>
2010-04-15 17:51:42 +0000

    create debian compatible packages with name-
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40283

--- openproximity2/create_dev_installer.sh
+++ openproximity2/create_dev_installer.sh
@@ -7,6 +7,6 @@ VERSION="devel-$(date +%Y%m%d%H%M%S)"
 cd $OP2
 echo $VERSION > openproximity2/latest-version
 echo $VERSION > ../devel-version
-
-tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$VERSION.tar.gz openproximity2
+mv openproximity2 openproximity2-$VERSION
+tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$VERSION.tar.gz openproximity2-$VERSION
 echo "openproximity2-$VERSION.tar.gz created"
--- openproximity2/create_release_installer.sh
+++ openproximity2/create_release_installer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+VERSION=$(cat $CWD/latest-version)
 source prepare_installer.sh
 cd $OP2
-tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$(cat $CWD/latest-version).tar.gz openproximity2
+mv openproximity2 openproximity2-$VERSION
+tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$VERSION.tar.gz openproximity2-$VERSION
