9498cd8bbfd2371a9e2df13396b42535a30f12cb
naranjo.manuel <none@none>
2010-07-08 17:51:03 +0000

    add OP twitter reader to main site
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40305

--- openproximity2/create_release_installer.sh
+++ openproximity2/create_release_installer.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-VERSION=$(cat $CWD/latest-version)
+CWD=$(pwd)
 source prepare_installer.sh
+
+VERSION=$(cat $CWD/latest-version)
 cd $OP2
 mv openproximity2 openproximity2-$VERSION
+
+echo "creating official release $VERSION" 
+
 tar --numeric-owner -h --group=0 --exclude=\*svn --owner=0 -czf $CWD/openproximity2-$VERSION.tar.gz openproximity2-$VERSION
