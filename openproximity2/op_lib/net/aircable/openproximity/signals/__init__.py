f184bc096346f47f02ab58d5b6aafd4f19c3919f
naranjo.manuel <none@none>
2010-08-06 15:15:56 +0000

    commented
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40337

--- openproximity2/op_lib/net/aircable/openproximity/signals/__init__.py
+++ openproximity2/op_lib/net/aircable/openproximity/signals/__init__.py
@@ -14,6 +14,10 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+'''
+Shared signals by OpenProximity rpc clients (scanner and uploader)
+'''
+
 import scanner
 import uploader
 
