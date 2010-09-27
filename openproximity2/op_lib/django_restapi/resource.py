8e485c0cfad3128660c17adc7651b6d04c979ef5
naranjo.manuel <none@none>
2010-09-27 22:01:45 +0000

    Added REST Api v2.0 this new library works similar to what django admin does it will generate html api docs
    new file:   op_lib/django_restapi/__init__.py
    new file:   op_lib/django_restapi/authentication.py
    new file:   op_lib/django_restapi/model_resource.py
    new file:   op_lib/django_restapi/receiver.py
    new file:   op_lib/django_restapi/resource.py
    new file:   op_lib/django_restapi/responder.py
    new file:   op_lib/django_restapi/sites.py
    new file:   op_lib/django_restapi/templates/rest/api_index.html
    new file:   op_lib/django_restapi/templates/rest/base.html
    new file:   op_lib/django_restapi/templates/rest/index.html
    new file:   op_lib/plugins/rest/rest.py
    modified:   op_lib/plugins/rest/urls.py
    
    --HG--
    extra : convert_revision : svn%3Ae9416b30-ef3e-0410-8207-55c012fc012f/trunk%40384

---
+++ openproximity2/op_lib/django_restapi/resource.py
@@ -0,0 +1,191 @@
+# -*- coding: utf-8 -*-
+"""
+Generic resource class.
+"""
+from django.utils.translation import ugettext as _
+from authentication import NoAuthentication
+from django.core.urlresolvers import reverse as reverse
+from django.http import Http404, HttpResponse, HttpResponseNotAllowed
+from django import template
+from django.shortcuts import render_to_response
+from django.views.decorators.cache import never_cache
+
+def load_put_and_files(request):
+    """
+    Populates request.PUT and request.FILES from
+    request.raw_post_data. PUT and POST requests differ 
+    only in REQUEST_METHOD, not in the way data is encoded. 
+    Therefore we can use Django's POST data retrieval method 
+    for PUT.
+    """
+    if request.method == 'PUT':
+        request.method = 'POST'
+        request._load_post_and_files()
+        request.method = 'PUT'
+        request.PUT = request.POST
+        del request._post
+
+class HttpMethodNotAllowed(Exception):
+    """
+    Signals that request.method was not part of
+    the list of permitted methods.
+    """
+
+class ResourceBase(object):
+    """
+    Base class for both model-based and non-model-based 
+    resources.
+    """
+    def __init__(self, authentication=None, permitted_methods=None):
+        """
+        authentication:
+            the authentication instance that checks whether a
+            request is authenticated
+        permitted_methods:
+            the HTTP request methods that are allowed for this 
+            resource e.g. ('GET', 'PUT')
+        """
+        # Access restrictions
+        if not authentication:
+            authentication = NoAuthentication()
+        self.authentication = authentication
+
+        if not permitted_methods:
+            permitted_methods = ["GET"]
+        self.permitted_methods = [m.upper() for m in permitted_methods]
+
+        self.api_index_template = None
+        self.api = False
+    
+    def dispatch(self, request, target, *args, **kwargs):
+        """
+        """
+        request_method = request.method.upper()
+        if request_method not in self.permitted_methods:
+            raise HttpMethodNotAllowed
+        
+        if request_method == 'GET':
+            return target.read(request, *args, **kwargs)
+        elif request_method == 'POST':
+            return target.create(request, *args, **kwargs)
+        elif request_method == 'PUT':
+            load_put_and_files(request)
+            return target.update(request, *args, **kwargs)
+        elif request_method == 'DELETE':
+            return target.delete(request, *args, **kwargs)
+        else:
+            raise Http404
+
+    def get_url(self):
+        """
+        Returns resource URL.
+        """
+        url = reverse('%s:index' % (self.site.name), current_app=self.site.name)
+        url += '%s/' % self.baseurl
+        print "ResourceBase.reverse", url
+        return url
+
+    def getAPI(self, request, extra_context=None):
+        if not self.api:
+            raise Exception("API generation not enabled")
+        
+        context = {
+                   'title': _('Rest API for %s' % self.name),
+                   'resource': self,
+                   'root_path': self.site.root_path
+        }
+        
+        if getattr(self, 'generateAPI', None):
+            context.update(self.generateAPI())
+        
+        context.update(extra_context or {})
+        context_instance = template.RequestContext(request, current_app=self.name)
+        return render_to_response(self.api_index_template or 'rest/api_index.html', context,
+                context_instance=context_instance)
+
+    getAPI = never_cache(getAPI)
+
+    def name(self):
+        raise NotImplementedError
+    name = property(name)
+
+    def baseurl(self):
+        raise NotImplementedError
+    baseurl = property(baseurl)
+
+    def get_urls(self):
+        from django.conf.urls.defaults import patterns, url
+
+        out =  patterns('',)
+        if self.api:
+            out+= patterns('', 
+        	url (r'^API/$', self.getAPI, name="API") 
+    	    )
+        out+= patterns('', 
+    		url(r'^([^/]*)/?$', self, name="index") 
+    	    )
+        return out
+
+    def urls(self):
+	print "Resource.urls", self.baseurl, self
+	return self.get_urls()
+    urls = property(urls)
+
+    # The four CRUD methods that any class that 
+    # inherits from Resource may implement:
+    
+    def create(self, request):
+        raise Http404
+    
+    def read(self, request):
+        raise Http404
+    
+    def update(self, request):
+        raise Http404
+    
+    def delete(self, request):
+        raise Http404
+
+class Resource(ResourceBase):
+    """
+    Generic resource class that can be used for
+    resources that are not based on Django models.
+    """
+    def __init__(self, authentication=None, permitted_methods=None,
+                 mimetype=None):
+        """
+        authentication:
+            the authentication instance that checks whether a
+            request is authenticated
+        permitted_methods:
+            the HTTP request methods that are allowed for this 
+            resource e.g. ('GET', 'PUT')
+        mimetype:
+            if the default None is not changed, any HttpResponse calls 
+            use settings.DEFAULT_CONTENT_TYPE and settings.DEFAULT_CHARSET
+        """
+        ResourceBase.__init__(self, authentication, permitted_methods)
+        self.mimetype = mimetype
+    
+    def __call__(self, request, *args, **kwargs):
+        """
+        Redirects to one of the CRUD methods depending 
+        on the HTTP method of the request. Checks whether
+        the requested method is allowed for this resource.
+        """
+        # Check permission
+        if not self.authentication.is_authenticated(request):
+            response = HttpResponse(_('Authorization Required'), mimetype=self.mimetype)
+            challenge_headers = self.authentication.challenge_headers()
+            for k,v in challenge_headers.items():
+                response[k] = v
+            response.status_code = 401
+            return response
+        
+        try:
+            return self.dispatch(request, self, *args, **kwargs)
+        except HttpMethodNotAllowed:
+            response = HttpResponseNotAllowed(self.permitted_methods)
+            response.mimetype = self.mimetype
+            return response
+    
--- openproximity2/op_lib/plugins/rest/urls.py
+++ openproximity2/op_lib/plugins/rest/urls.py
@@ -1,52 +1,10 @@
-# -*- coding: utf-8 -*-
-# REST interface for OpenProximity
-# Copyright (C) 2010 Manuel Coli <manuel@polline.net>
-# Copyright (C) 2010,2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+from django.conf.urls.defaults import patterns, include
+import django_restapi as restapi
 
-from django.conf.urls.defaults import *
-import views
+restapi.autodiscover()
+import rest
 
-urlpatterns = patterns('',
-    url(r'MarketingCampaign/(.*?)/?$', 
-	  views.json_MarketingCampaign_resource),
-    url(r'CampaignFile/get/(?P<pk>\d+)/?$', 
-	  views.grab_file),
-    url(r'CampaignFile/(.*?)/?$', 
-	  views.json_CampaignFile_resource),
-    url(r'CampaignFileLast/(.*?)/?$', 
-	  views.json_CampaignFile_last_resource),
-    url(r'RemoteDevice/(.*?)/?$', 
-	  views.json_RemoteDevice_resource),
-    url(r'DeviceRecord/(.*?)/?$', 
-	  views.json_DeviceRecord_resource),
-    url(r'RemoteBluetoothDeviceRecord/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceRecord_resource),
-    url(r'RemoteBluetoothDeviceFoundRecord/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceFoundRecord_resource),
-    url(r'RemoteBluetoothDeviceSDP/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceSDP_resource),
-    url(r'RemoteBluetoothDeviceNoSDP/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceNoSDP_resource),
-    url(r'RemoteBluetoothDeviceSDPTimeout/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceSDPTimeout_resource),
-    url(r'RemoteBluetoothDeviceFilesRejected/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceFilesRejected_resource),
-    url(r'RemoteBluetoothDeviceFilesSuccess/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceFilesSuccess_resource),
-    url(r'RemoteBluetoothDeviceFileTry/(.*?)/?$', 
-	  views.json_RemoteBluetoothDeviceFileTry_resource),
-    url(r'.*', views.index)
+urlpatterns = patterns ('',
+    # Uncomment the next line to enable the admin:
+    (r'', include(restapi.site.urls)),
 )
