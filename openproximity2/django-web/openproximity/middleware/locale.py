f888ced40a66ce1e4b1d94494b220259430430ab
Naranjo Manuel Francisco <manuel@aircable.net>
2010-12-16 17:22:10 -0300

    Fixed localization bugs

--- openproximity2/django-web/openproximity/middleware/locale.py
+++ openproximity2/django-web/openproximity/middleware/locale.py
@@ -3,10 +3,11 @@
 from django.utils.cache import patch_vary_headers
 from django.utils import translation
 from django.middleware.locale import LocaleMiddleware
+from net.aircable.utils import logger
 
-class ILocaleMiddleware(object):
+class ILocaleMiddleware(LocaleMiddleware):
     def process_request(self, request):
 	try:
 	    super(ILocaleMiddleware, self).process_request(request)
-	except:
-	    pass
+	except  Exception, err:
+	    logger.exception(err)
