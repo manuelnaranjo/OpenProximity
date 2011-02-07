109ebe3b89c303e6a2f91aa5b1ab36b672e48e62
Naranjo Manuel Francisco <manuel@aircable.net>
2011-02-07 19:54:09 -0300

    Fixed egg command

--- openproximity2/django-web/openproximity/management/commands/egg_command.py
+++ openproximity2/django-web/openproximity/management/commands/egg_command.py
@@ -19,8 +19,7 @@ from django.conf import settings
 from django.utils.importlib import import_module
 
 class Command(BaseCommand):
-    help = "try running a command inside an egg, pass as argument the wanted "
-            "command"
+    help = "try running a command inside an egg, pass as argument the wanted command"
     
     def handle(self, command="", *args, **kwargs):
 	for app in settings.INSTALLED_APPS:
