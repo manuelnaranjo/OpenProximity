b018c7e22afb201d965eac1035901727eb63a30e
Manuel Francisco Naranjo <manuel@aircable.net>
2011-09-30 09:41:43 -0700

    improved agent to be compatible with in-process pairing

--- openproximity2/serverXR/agent.py
+++ openproximity2/serverXR/agent.py
@@ -1,5 +1,3 @@
- #!/usr/bin/python
-# -*- coding: utf-8 -*-
 # OpenProximity2.0 is a proximity marketing OpenSource system.
 # Copyright (C) 2010,2009,2008 Naranjo Manuel Francisco <manuel@aircable.net>
 #
@@ -18,104 +16,21 @@
 
 
 '''
-An advanced pairing manager that will connect to OpenProximity rpc server in to
-ask which PIN code to use.
-
-SPP mode not yet supported.
+OpenProximity pairing agent, this agent is ment to be run along our uploader
+process.
 '''
-import setpaths
-
 import gobject
 import sys, os
-import dbus, dbus.service, dbus.mainloop.glib
+import dbus, dbus.service
 import rpyc
 
 from net.aircable.utils import getLogger
 logger = getLogger(__name__)
 
-DEFAULT_PIN=os.environ.get("PIN_CODE", "1234")
-# default pin to use when no server is available.
-
-logger.info("PIN code defaulting to %s" % DEFAULT_PIN)
-server = None
-
-PATH="/net/aircable/pairing"
+PATH="/net/aircable/agent"
 # Path where we register our pairing agent.
 
-def connect(address, port):
-    '''
-    This method wraps rpyc server connection, so when no server is available 
-    the pairing manager will still work.
-    '''
-    logger.info("Connecting to %s:%s" % (address, port))
-    try:
-        s = rpyc.connect(address, int(port))
-        return s
-    except Exception, err:
-        logger.error("can't connect to server")
-        logger.exception(err)
-    return None
-
-def getPIN(address, dongle):
-    '''
-    This method will try to ask the server which PIN code to use, otherwise 
-    fallback to the default PIN.
-    '''
-    global server
-    if len(sys.argv) > 2:
-        logger.info("server available")
-        if not server:
-            logger.info("server available")
-            server=connect(sys.argv[1], sys.argv[2])
-        try:
-            out = server.root.getPIN(address, dongle)
-            return str(out)
-        except Exception, err:
-            logger.error("couldn't get PIN from server")
-            logger.exception(err)
-    logger.info("faulting to default pin")
-    return DEFAULT_PIN
-
-def handle_name_owner_changed(own, old, new):
-    '''
-    When ever bluez goes up or down we will get called, we don't register here 
-    though, we do it on handle_adapter_added as that's when we know which 
-    dongles nare available.
-    '''
-    if not own.startswith('org.bluez'):
-	    return
-    
-    if new is None or len(str(new))==0:
-        logger.info( "bluez has gone down, time to get out")
-	else:
-        logger.info( "bluez started, time to restart")
-    
-
-def registerAgent(path):
-    '''
-    Register an agent on the given dbus path.
-    '''
-    adapter = dbus.Interface(bus.get_object("org.bluez", path),
-                        "org.bluez.Adapter")
-    adapter.RegisterAgent(PATH, "NoInputNoOutput") 
-    # should we change this to NoInputNoOutput?
-    logger.info("adapter registered for path %s" % path)
-
-
-def handle_adapter_added(path, signal):
-    '''
-    When ever a new dongle is attached, or bluez starts a signal is generated, 
-    that's when we register the agent for that adapter.
-    '''
-    logger.info("bluez.%s: %s" % (signal, path))
-    registerAgent(path)
-
-def handle_adapter_removed(path, signal):
-    '''
-    When ever a dongle is removed we will get notified, as we only run with one
-    agent instance we have nothing to do.
-    '''
-    logger.info("adapter removed %s" % path)
+bus = dbus.SystemBus()
 
 class Rejected(dbus.DBusException):
     '''
@@ -129,10 +44,10 @@ class Agent(dbus.service.Object):
     
     Documentation for the methods come from BlueZ docs.
     '''
-    exit_on_release = True
 
-    def set_exit_on_release(self, exit_on_release):
-        self.exit_on_release = exit_on_release
+    def __init__(self, rpc_server, *a, **kw):
+        self.rpc_server = rpc_server
+        dbus.service.Object.__init__(self, *a, **kw)
 
     @dbus.service.method("org.bluez.Agent", in_signature="", out_signature="")
     def Release(self):
@@ -141,9 +56,6 @@ class Agent(dbus.service.Object):
         needs us to release the agent.
         '''
         logger.info("Agent Release")
-        if self.exit_on_release:
-	        logger.info("Exiting from loop")
-	        mainloop.quit()
 
     @dbus.service.method("org.bluez.Agent", in_signature="os", out_signature="")
     def Authorize(self, device, uuid):
@@ -163,16 +75,21 @@ class Agent(dbus.service.Object):
         The return value should be a string of 1-16 characters length. The 
         string can be alphanumeric.
         '''
-        device = dbus.Interface(bus.get_object("org.bluez", path), 
+        logger.info("request pin code")
+        try:
+	        device = dbus.Interface(bus.get_object("org.bluez", path), 
                             "org.bluez.Device")
-        dongle = dbus.Interface(bus.get_object(
+	        dongle = dbus.Interface(bus.get_object(
 		        "org.bluez", device.GetProperties()['Adapter']),
-	        "org.bluez.Adapter")
-        device=str(device.GetProperties()['Address'])
-        dongle=str(dongle.GetProperties()['Address'])
-        pin=getPIN(device, dongle)
-        logger.info("RequestPinCode (%s->%s): %s" % (dongle, device, pin) )
-        return pin
+                                "org.bluez.Adapter")
+	        device=str(device.GetProperties()['Address'])
+	        dongle=str(dongle.GetProperties()['Address'])
+	        pin=self.rpc_server.root.getPIN(device, dongle)
+	        logger.info("RequestPinCode (%s->%s): %s" % (dongle, device, pin) )
+	        return pin
+        except Exception, err:
+	        logger.error(err)
+	        return "1234"
 
     @dbus.service.method("org.bluez.Agent", in_signature="o", out_signature="u")
     def RequestPasskey(self, device):
@@ -182,11 +99,12 @@ class Agent(dbus.service.Object):
 
         The return value should be a numeric value between 0-999999.
         '''
+        logger.info("RequestPasskey")
         logger.info("RequestPasskey (%s): %s" % (device, PIN) )
         return dbus.UInt32(PIN)
 
     @dbus.service.method("org.bluez.Agent", in_signature="ou", out_signature="")
-    def DisplayPasskey(self, device, passkey):
+    def DisplayPasskey(self, device, value):
         '''
         This method gets called when the service daemon needs to display a 
         passkey for an authentication.
@@ -200,7 +118,7 @@ class Agent(dbus.service.Object):
         During the pairing process this method might be called multiple times 
         to update the entered value.
         '''
-        logger.info("DisplayPasskey (%s, %d)" % (device, passkey))
+        logger.info("DisplayPasskey (%s, %s)" % (device, value))
 
     @dbus.service.method("org.bluez.Agent", in_signature="ou", out_signature="")
     def RequestConfirmation(self, device, passkey):
@@ -211,7 +129,7 @@ class Agent(dbus.service.Object):
         To confirm the value it should return an empty reply or an error in 
         case the passkey is invalid.
         '''
-        logger.info("RequestConfirmation (%s, %d)" % (device, passkey))
+        logger.info("RequestConfirmation (%s, %s)" % (device, passkey))
         device = dbus.Interface(bus.get_object("org.bluez", device),
                         "org.bluez.Device")
         dongle = dbus.Interface(bus.get_object("org.bluez", 
@@ -219,10 +137,10 @@ class Agent(dbus.service.Object):
                 "org.bluez.Adapter")
         device=str(device.GetProperties()['Address'])
         dongle=str(dongle.GetProperties()['Address'])
-        pin=getPIN(device, dongle)
+        pin=self.rpc_server.root.getPIN(device, dongle)
         if passkey == pin:
-	        logger.info("passkey matches")
-	        return
+            logger.info("passkey matches")
+            return
         logger.info("passkey doesn't match")
         raise Rejected("Passkey doesn't match")
 
@@ -241,49 +159,3 @@ class Agent(dbus.service.Object):
         before a reply was returned.
         '''
         logger.info("Cancel")
-
-def registerControlSignals(bus):
-    '''
-    Register our selves with some important dbus signals.
-    '''
-    bus.add_signal_receiver(handle_name_owner_changed,
-            'NameOwnerChanged',
-            'org.freedesktop.DBus',
-            'org.freedesktop.DBus',
-            '/org/freedesktop/DBus')
-
-    bus.add_signal_receiver(handle_adapter_added,
-            signal_name='AdapterAdded',
-            dbus_interface='org.bluez.Manager',
-            member_keyword='signal')
-
-    bus.add_signal_receiver(handle_adapter_removed,
-            signal_name='AdapterRemoved',
-            dbus_interface='org.bluez.Manager',
-            member_keyword='signal')
-
-def initAgent():
-    '''
-    Initialize the agent, register with every available path if possible.
-    '''
-    try:
-		manager = dbus.Interface(bus.get_object("org.bluez", "/"),
-                                    "org.bluez.Manager")
-		for path in manager.ListAdapters():
-			registerAgent(path)
-            logger.info("Agent registered on all paths")
-    except Exception, err:
-		logger.error("Something went wrong on the agent application")
-        logger.exception(err)
-
-if __name__ == '__main__':
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-
-    bus = dbus.SystemBus()
-    registerControlSignals(bus)
-    mainloop = gobject.MainLoop()
-    agent = Agent(bus, PATH)
-    agent.set_exit_on_release(False)
-    initAgent()
-    mainloop.run()
-    logger.info("Agent is exiting")
